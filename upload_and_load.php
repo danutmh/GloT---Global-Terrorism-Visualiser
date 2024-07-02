4ad3022a9ff146bc5ded8439dd27c3a047d5ad12
danutmh <danutmh@yahoo.com>
2024-07-02 18:56:51 +0300

    Prepared statements for data insertion
    no more errors

--- upload_and_load.php
+++ upload_and_load.php
@@ -24,186 +24,98 @@ if ($_FILES["file"]["error"] == UPLOAD_ERR_OK) {
             $headers = fgetcsv($handle, 50000, ',');
 
             // Loop through each row of the file
-            while (($row = fgetcsv($handle, 50000, ',')) !== FALSE) {
-                // Map the row data to the respective columns
-				$eventid = $row[0];
-				$iyear = $row[1];
-				$imonth = $row[2];
-				$iday = $row[3];
-				$approxdate = $row[4];
-				$extended = $row[5];
-				$resolution = $row[6];
-				$country = $row[7];
-				$country_txt = $row[8];
-				$region = $row[9];
-				$region_txt = $row[10];
-				$provstate = $row[11];
-				$city = $row[12];
-				$latitude = $row[13];
-				$longitude = $row[14];
-				$specificity = $row[15];
-				$vicinity = $row[16];
-				$location = $row[17];
-				$summary = $row[18];
-				$crit1 = $row[19];
-				$crit2 = $row[20];
-				$crit3 = $row[21];
-				$doubtterr = $row[22];
-				$alternative = $row[23];
-				$alternative_txt = $row[24];
-				$multiple = $row[25];
-				$success = $row[26];
-				$suicide = $row[27];
-				$attacktype1 = $row[28];
-				$attacktype1_txt = $row[29];
-				$attacktype2 = $row[30];
-				$attacktype2_txt = $row[31];
-				$attacktype3 = $row[32];
-				$attacktype3_txt = $row[33];
-				$targtype1 = $row[34];
-				$targtype1_txt = $row[35];
-				$targsubtype1 = $row[36];
-				$targsubtype1_txt = $row[37];
-				$corp1 = $row[38];
-				$target1 = $row[39];
-				$natlty1 = $row[40];
-				$natlty1_txt = $row[41];
-				$targtype2 = $row[42];
-				$targtype2_txt = $row[43];
-				$targsubtype2 = $row[44];
-				$targsubtype2_txt = $row[45];
-				$corp2 = $row[46];
-				$target2 = $row[47];
-				$natlty2 = $row[48];
-				$natlty2_txt = $row[49];
-				$targtype3 = $row[50];
-				$targtype3_txt = $row[51];
-				$targsubtype3 = $row[52];
-				$targsubtype3_txt = $row[53];
-				$corp3 = $row[54];
-				$target3 = $row[55];
-				$natlty3 = $row[56];
-				$natlty3_txt = $row[57];
-				$gname = $row[58];
-				$gsubname = $row[59];
-				$gname2 = $row[60];
-				$gsubname2 = $row[61];
-				$gname3 = $row[62];
-				$gsubname3 = $row[63];
-				$motive = $row[64];
-				$guncertain1 = $row[65];
-				$guncertain2 = $row[66];
-				$guncertain3 = $row[67];
-				$individual = $row[68];
-				$nperps = $row[69];
-				$nperpcap = $row[70];
-				$claimed = $row[71];
-				$claimmode = $row[72];
-				$claimmode_txt = $row[73];
-				$claim2 = $row[74];
-				$claimmode2 = $row[75];
-				$claimmode2_txt = $row[76];
-				$claim3 = $row[77];
-				$claimmode3 = $row[78];
-				$claimmode3_txt = $row[79];
-				$compclaim = $row[80];
-				$weaptype1 = $row[81];
-				$weaptype1_txt = $row[82];
-				$weapsubtype1 = $row[83];
-				$weapsubtype1_txt = $row[84];
-				$weaptype2 = $row[85];
-				$weaptype2_txt = $row[86];
-				$weapsubtype2 = $row[87];
-				$weapsubtype2_txt = $row[88];
-				$weaptype3 = $row[89];
-				$weaptype3_txt = $row[90];
-				$weapsubtype3 = $row[91];
-				$weapsubtype3_txt = $row[92];
-				$weaptype4 = $row[93];
-				$weaptype4_txt = $row[94];
-				$weapsubtype4 = $row[95];
-				$weapsubtype4_txt = $row[96];
-				$weapdetail = $row[97];
-				$nkill = $row[98];
-				$nkillus = $row[99];
-				$nkillter = $row[100];
-				$nwound = $row[101];
-				$nwoundus = $row[102];
-				$nwoundte = $row[103];
-				$property = $row[104];
-				$propextent = $row[105];
-				$propextent_txt = $row[106];
-				$propvalue = $row[107];
-				$propcomment = $row[108];
-				$ishostkid = $row[109];
-				$nhostkid = $row[110];
-				$nhostkidus = $row[111];
-				$nhours = $row[112];
-				$ndays = $row[113];
-				$divert = $row[114];
-				$kidhijcountry = $row[115];
-				$ransom = $row[116];
-				$ransomamt = $row[117];
-				$ransomamtus = $row[118];
-				$ransompaid = $row[119];
-				$ransompaidus = $row[120];
-				$ransomnote = $row[121];
-				$hostkidoutcome = $row[122];
-				$hostkidoutcome_txt = $row[123];
-				$nreleased = $row[124];
-				$addnotes = $row[125];
-				$scite1 = $row[126];
-				$scite2 = $row[127];
-				$scite3 = $row[128];
-				$dbsource = $row[129];
-				$INT_LOG = $row[130];
-				$INT_IDEO = $row[131];
-				$INT_MISC = $row[132];
-				$INT_ANY = $row[133];
-				$related = $row[134];
+            $eventStmt = $conn->prepare("INSERT INTO events (
+                eventid, iyear, imonth, iday, approxdate, extended, resolution, summary, crit1, crit2, crit3, doubtterr, 
+                alternative, alternative_txt, multiple, related
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-                // Insert data into the events table
-                $sql_events = "INSERT INTO events (eventid, iyear, imonth, iday, approxdate, extended, resolution, summary, crit1, crit2, crit3, doubtterr, alternative, alternative_txt, multiple, related) VALUES ('$eventid', '$iyear', '$imonth', '$iday', '$approxdate', '$extended', '$resolution', '$summary', '$crit1', '$crit2', '$crit3', '$doubtterr', '$alternative', '$alternative_txt', '$multiple', '$related')";
-                $conn->query($sql_events);
-			
-				$sql_location = "INSERT INTO location (eventid, country, country_txt, region, region_txt, provstate, city, latitude, longitude, specificity, vicinity, location)
-					 VALUES ('$eventid', '$country', '$country_txt', '$region', '$region_txt', '$provstate', '$city', '$latitude', '$longitude', '$specificity', '$vicinity', '$location')";
-				$conn->query($sql_location);
+            $locationStmt = $conn->prepare("INSERT INTO location (
+                eventid, country, country_txt, region, region_txt, provstate, city, latitude, longitude, specificity, 
+                vicinity, location
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-				// Insert data into the attack table
-				$sql_attack = "INSERT INTO attack (eventid, attacktype1, attacktype1_txt, attacktype2, attacktype2_txt, attacktype3, attacktype3_txt, success, suicide)
-							   VALUES ('$eventid', '$attacktype1', '$attacktype1_txt', '$attacktype2', '$attacktype2_txt', '$attacktype3', '$attacktype3_txt', '$success', '$suicide')";
-				$conn->query($sql_attack);
-				
-				$sql_casualties = "INSERT INTO casualties (eventid, nkill, nkillus, nkillter, nwound, nwoundus, nwoundte)
-								   Values ('$eventid', '$nkill', '$nkillus', '$nkillter', '$nwound' , '$nwoundus', '$nwoundte')";
-				$conn->query($sql_casualties);
-				
-				
-				$sql_hostage_kidnapping = "INSERT INTO attack (eventid, ishostkid, nhostkid, nhostkidus, nhours, ndays, divert, kidhijcountry, ransom, ransomamt, ransomamtus, ransompaid, ransompaidus, ransomnote, hostkidoutcome, hostkidoutcome_txt, nreleased)
-							   VALUES ('$eventid', '$ishostkid', '$nhostkid', '$nhostkidus', '$nhours', '$ndays', '$divert', '$kidhijcountry', '$ransom', '$ransomamt', '$ransomamtus, '$ransompaid', '$ransompaidus', '$ransomnote', '$hostkidoutcome', '$hostkidoutcome_txt', '$nreleased')";
-				$conn->query($sql_hostage_kidnapping);
-				
-				$sql_perpetrator = "INSERT INTO attack (eventid, gname, gsubname, gname2, gsubname2, gname3, gsubname3, motive, guncertain1, guncertain2, guncertain3, individual, nperps, nperpcap)
-							   VALUES ('$eventid', '$gname', '$gsubname', '$gname2', '$gsubname2', '$gname3', '$gsubname3', '$motive', '$guncertain1', '$guncertain2', '$guncertain3', '$individual', '$nperps', '$nperpcap')";
-				$conn->query($sql_perpetrator);
-				
-				$sql_property_damage = "INSERT INTO attack (eventid, property, propextent,propextent_txt ,propvalue ,propcomment)
-							   VALUES ('$eventid', '$property', '$propextent','$propextent_txt','$propvalue','$propcomment')";
-				$conn->query($sql_property_damage);
-				
-				$sql_target = "INSERT INTO attack (eventid, targtype1, targtype1_txt, targsubtype1, targsubtype1_txt, corp1, target1, natlty1, natlty1_txt, targtype2, targtype2_txt, targsubtype2, targsubtype2_txt, corp2, target2, natlty2, natlty2_txt, targtype3, targtype3_txt, targsubtype3, targsubtype3_txt, corp3, target3, natlty3, natlty3_txt)
-							   VALUES ('$eventid', '$targtype1', '$targtype1_txt', '$targsubtype1', '$targsubtype1_txt', '$corp1', '$target1', '$natlty1', '$natlty1_txt', '$targtype2', '$targtype2_txt', '$targsubtype2', '$targsubtype2_txt', '$corp2', '$target2', '$natlty2', '$natlty2_txt', '$targtype3', '$targtype3_txt', '$targsubtype3', '$targsubtype3_txt', '$corp3', '$target3', '$natlty3', '$natlty3_txt')";
-				$conn->query($sql_target);
-				
-				$sql_weapon = "INSERT INTO attack (eventid, weaptype1, weaptype1_txt, weapsubtype1, weapsubtype1_txt, weaptype2, weaptype2_txt, weapsubtype2, weapsubtype2_txt, weaptype3, weaptype3_txt, weapsubtype3, weapsubtype3_txt, weaptype4, weaptype4_txt, weapsubtype4, weapsubtype4_txt, weapdetail)
-							   VALUES ('$eventid', '$weaptype1', '$weaptype1_txt', '$weapsubtype1', '$weapsubtype1_txt', '$weaptype2', '$weaptype2_txt', '$weapsubtype2', '$weapsubtype2_txt', '$weaptype3', '$weaptype3_txt', '$weapsubtype3', '$weapsubtype3_txt', '$weaptype4', '$weaptype4_txt', '$weapsubtype4', '$weapsubtype4_txt', '$weapdetail')";
-				$conn->query($sql_weapon);
-				
-				$sql_claim = "INSERT INTO attack (eventid, claimed, claimmode, claimmode_txt, claim2, claimmode2, claimmode2_txt, claim3, claimmode3, claimmode3_txt, compclaim)
-							   VALUES ('$eventid', '$claimed', '$claimmode', '$claimmode_txt', '$claim2', '$claimmode2', '$claimmode2_txt', '$claim3', '$claimmode3', '$claimmode3_txt', '$compclaim')";
-				$conn->query($sql_claim);
+            $attackStmt = $conn->prepare("INSERT INTO attack (
+                eventid, attacktype1, attacktype1_txt, attacktype2, attacktype2_txt, attacktype3, attacktype3_txt, 
+                success, suicide
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+            $targetStmt = $conn->prepare("INSERT INTO target (
+                eventid, targtype1, targtype1_txt, targsubtype1, targsubtype1_txt, corp1, target1, natlty1, natlty1_txt, 
+                targtype2, targtype2_txt, targsubtype2, targsubtype2_txt, corp2, target2, natlty2, natlty2_txt, 
+                targtype3, targtype3_txt, targsubtype3, targsubtype3_txt, corp3, target3, natlty3, natlty3_txt
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+            $perpetratorStmt = $conn->prepare("INSERT INTO perpetrator (
+                eventid, gname, gsubname, gname2, gsubname2, gname3, gsubname3, motive, guncertain1, guncertain2, 
+                guncertain3, individual, nperps, nperpcap
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+            $claimStmt = $conn->prepare("INSERT INTO claim (
+                eventid, claimed, claimmode, claimmode_txt, claim2, claimmode2, claimmode2_txt, claim3, claimmode3, 
+                claimmode3_txt, compclaim
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+            $weaponStmt = $conn->prepare("INSERT INTO weapon (
+                eventid, weaptype1, weaptype1_txt, weapsubtype1, weapsubtype1_txt, weaptype2, weaptype2_txt, 
+                weapsubtype2, weapsubtype2_txt, weaptype3, weaptype3_txt, weapsubtype3, weapsubtype3_txt, weaptype4, 
+                weaptype4_txt, weapsubtype4, weapsubtype4_txt, weapdetail
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+            $casualtiesStmt = $conn->prepare("INSERT INTO casualties (
+                eventid, nkill, nkillus, nkillter, nwound, nwoundus, nwoundte
+            ) VALUES (?, ?, ?, ?, ?, ?, ?)");
+
+            $propertyStmt = $conn->prepare("INSERT INTO property_damage (
+                eventid, property, propextent, propextent_txt, propvalue, propcomment
+            ) VALUES (?, ?, ?, ?, ?, ?)");
+
+            $hostageStmt = $conn->prepare("INSERT INTO hostage_kidnapping (
+                eventid, ishostkid, nhostkid, nhostkidus, nhours, ndays, divert, kidhijcountry, ransom, ransomamt, 
+                ransomamtus, ransompaid, ransompaidus, ransomnote, hostkidoutcome, hostkidoutcome_txt, nreleased
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+            while (($row = fgetcsv($handle, 5000, ",")) !== FALSE) {
+                // Insert into events table
+                $eventStmt->bind_param("iiissisiiiibiisi", $row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6], $row[18], $row[19], $row[20], $row[21], $row[22], $row[23], $row[24], $row[25], $row[127]);
+                $eventStmt->execute();
+
+                // Insert into location table
+                $locationStmt->bind_param("iisisssddiis", $row[0], $row[7], $row[8], $row[9], $row[10], $row[11], $row[12], $row[13], $row[14], $row[15], $row[16], $row[17]);
+                $locationStmt->execute();
+
+                // Insert into attack table
+                $attackStmt->bind_param("iisiisiib", $row[0], $row[27], $row[28], $row[29], $row[30], $row[31], $row[32], $row[26], $row[35]);
+                $attackStmt->execute();
+
+                // Insert into target table
+                $targetStmt->bind_param("iisisssisisisssisisisssis", $row[0], $row[34], $row[35], $row[36], $row[37], $row[38], $row[39], $row[40], $row[41], $row[42], $row[43], $row[44], $row[45], $row[46], $row[47], $row[48], $row[49], $row[50], $row[51], $row[52], $row[53], $row[54], $row[55], $row[56], $row[57]);
+                $targetStmt->execute();
+
+                // Insert into perpetrator table
+                $perpetratorStmt->bind_param("isssssssiiiiii", $row[0], $row[57], $row[58], $row[59], $row[60], $row[61], $row[62], $row[63], $row[64], $row[65], $row[66], $row[67], $row[68], $row[69]);
+                $perpetratorStmt->execute();
+
+                // Insert into claim table
+                $claimStmt->bind_param("iiisiisiisi", $row[0], $row[70], $row[71], $row[72], $row[73], $row[74], $row[75], $row[76], $row[77], $row[78], $row[79]);
+                $claimStmt->execute();
+
+                // Insert into weapon table
+                $weaponStmt->bind_param("iisisisisisisisiss", $row[0], $row[80], $row[81], $row[82], $row[83], $row[84], $row[85], $row[86], $row[87], $row[88], $row[89], $row[90], $row[91], $row[92], $row[93], $row[94], $row[95], $row[96]);
+                $weaponStmt->execute();
+
+                // Insert into casualties table
+                $casualtiesStmt->bind_param("iiiiiii", $row[0], $row[97], $row[98], $row[99], $row[100], $row[101], $row[102]);
+                $casualtiesStmt->execute();
+
+                // Insert into property table
+                $propertyStmt->bind_param("iiisds", $row[0], $row[103], $row[104], $row[105], $row[106], $row[107]);
+                $propertyStmt->execute();
+
+                // Insert into hostage table
+                $hostageStmt->bind_param("iiiiiissiddddsisi", $row[0], $row[108], $row[109], $row[110], $row[111], $row[112], $row[113], $row[114], $row[115], $row[116], $row[117], $row[118], $row[119], $row[120], $row[121], $row[122], $row[123]);
+                $hostageStmt->execute();
             }
+
             fclose($handle);
         }
         echo "Data loaded successfully.";
